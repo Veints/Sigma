58bf9ee74d8004c4e9c3e54d2916666bff5f275f
Keller <93058465+Veints@users.noreply.github.com>
2024-10-28 17:45:42 -0700

    Update player.js

--- js/player.js
+++ js/player.js
@@ -10,9 +10,10 @@ class Player {
         this.jumpPower = -10;
         this.velocityY = 0;
         this.isJumping = false;
-        this.wobble = 0;
-        this.wobbleSpeed = 0.1;
+        this.leanAngle = 0; // Leaning angle
+        this.leanDirection = 0; // 1 for right, -1 for left
         this.knockback = 0;
+        this.onPlatform = true;
     }
 
     update() {
@@ -20,39 +21,48 @@ class Player {
         this.x += this.knockback;
         this.knockback *= 0.9; // Dampen knockback over time
 
-        // Wobble movement
-        this.wobble += this.wobbleSpeed;
-        this.x += Math.sin(this.wobble) * 2; // Wobble effect
+        // Update leaning
+        this.leanAngle += this.leanDirection * 0.1;
 
         // Gravity and jumping
-        this.y += this.velocityY;
-        this.velocityY += this.gravity;
+        if (this.onPlatform) {
+            this.y += this.velocityY;
+            this.velocityY += this.gravity;
 
-        if (this.y + this.height >= canvas.height) {
-            this.y = canvas.height - this.height;
-            this.isJumping = false;
-            this.velocityY = 0;
+            // Check if on the platform
+            if (this.y + this.height >= canvas.height - 100) {
+                this.y = canvas.height - 100 - this.height; // Adjust to be on top of the platform
+                this.isJumping = false;
+                this.velocityY = 0;
+            }
+        }
+
+        // Check for falling off the platform
+        if (this.y > canvas.height) {
+            this.onPlatform = false; // Player has fallen off
         }
 
         this.handleInput();
     }
 
     handleInput() {
-        if (this.controls.jump && !this.isJumping) {
+        if (this.controls.jump && this.onPlatform) {
             this.velocityY = this.jumpPower;
-            this.isJumping = true;
-            this.velocityY += (Math.sin(this.wobble) * -5); // Jump direction based on wobble
+            this.velocityY += (this.leanDirection * -5); // Jump direction based on lean
+            this.onPlatform = false;
         }
     }
 
     draw(ctx) {
         ctx.fillStyle = this.color;
         // Draw a simple person shape
-        ctx.beginPath();
-        ctx.arc(this.x + this.width / 2, this.y + 15, 10, 0, Math.PI * 2); // Head
-        ctx.fillRect(this.x, this.y + 10, this.width, this.height - 10); // Body
+        ctx.save();
+        ctx.translate(this.x + this.width / 2, this.y + 15);
+        ctx.rotate(this.leanAngle);
+        ctx.fillRect(-this.width / 2, -15, this.width, this.height); // Body
         ctx.fillStyle = 'black'; // Legs
-        ctx.fillRect(this.x, this.y + this.height - 10, this.width / 2, 10);
-        ctx.fillRect(this.x + this.width / 2, this.y + this.height - 10, this.width / 2, 10);
+        ctx.fillRect(-this.width / 2, 0, this.width / 2, 10);
+        ctx.fillRect(0, 0, this.width / 2, 10);
+        ctx.restore();
     }
 }
