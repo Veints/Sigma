c2d6281550987e1626b8dd78adef0d27be9f17bb
Keller <93058465+Veints@users.noreply.github.com>
2024-10-28 17:31:45 -0700

    Update player.js

--- js/player.js
+++ js/player.js
@@ -10,13 +10,24 @@ class Player {
         this.jumpPower = -10;
         this.velocityY = 0;
         this.isJumping = false;
+        this.wobble = 0;
+        this.wobbleSpeed = 0.1;
+        this.knockback = 0;
     }
 
     update() {
+        // Apply knockback
+        this.x += this.knockback;
+        this.knockback *= 0.9; // Dampen knockback over time
+
+        // Wobble movement
+        this.wobble += this.wobbleSpeed;
+        this.x += Math.sin(this.wobble) * 2; // Wobble effect
+
+        // Gravity and jumping
         this.y += this.velocityY;
         this.velocityY += this.gravity;
 
-        // Simple ground collision
         if (this.y + this.height >= canvas.height) {
             this.y = canvas.height - this.height;
             this.isJumping = false;
@@ -30,11 +41,18 @@ class Player {
         if (this.controls.jump && !this.isJumping) {
             this.velocityY = this.jumpPower;
             this.isJumping = true;
+            this.velocityY += (Math.sin(this.wobble) * -5); // Jump direction based on wobble
         }
     }
 
     draw(ctx) {
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // Draw a simple person shape
+        ctx.beginPath();
+        ctx.arc(this.x + this.width / 2, this.y + 15, 10, 0, Math.PI * 2); // Head
+        ctx.fillRect(this.x, this.y + 10, this.width, this.height - 10); // Body
+        ctx.fillStyle = 'black'; // Legs
+        ctx.fillRect(this.x, this.y + this.height - 10, this.width / 2, 10);
+        ctx.fillRect(this.x + this.width / 2, this.y + this.height - 10, this.width / 2, 10);
     }
 }
