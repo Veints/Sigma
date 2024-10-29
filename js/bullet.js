0349d0b3a292ffc2771bb8e3eaf8b637f04f8844
Keller <93058465+Veints@users.noreply.github.com>
2024-10-28 17:45:58 -0700

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -1,13 +1,15 @@
 class Bullet {
-    constructor(x, y, direction) {
+    constructor(x, y, direction, angle) {
         this.x = x;
         this.y = y;
         this.size = 5;
-        this.speed = 8 * direction; // Increase speed
+        this.speed = 8 * direction; // Speed of bullet
+        this.angle = angle;
     }
 
     update() {
-        this.x += this.speed;
+        this.x += this.speed * Math.cos(this.angle);
+        this.y += this.speed * Math.sin(this.angle);
     }
 
     draw(ctx) {
