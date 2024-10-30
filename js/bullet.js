e83aab94b322f4bce83940b98a5ea60467f2d4a7
Keller <93058465+Veints@users.noreply.github.com>
2024-10-29 20:53:10 -0700

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -1,23 +1,23 @@
 class Bullet {
-    constructor(x, y, direction, angle) {
+    constructor(x, y, angle) {
         this.x = x;
         this.y = y;
-        this.direction = direction;
+        this.radius = 5;
         this.speed = 10; // Speed of the bullet
-        this.radius = 5; // Bullet size
-        this.angle = angle; // Angle of bullet trajectory
+        this.angle = angle;
+        this.dx = Math.cos(angle) * this.speed;
+        this.dy = Math.sin(angle) * this.speed;
     }
 
     update() {
-        // Update position based on speed and angle
-        this.x += this.speed * this.direction * Math.cos(this.angle);
-        this.y += this.speed * this.direction * Math.sin(this.angle);
+        this.x += this.dx;
+        this.y += this.dy;
     }
 
-    draw(ctx) {
-        ctx.fillStyle = 'grey';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
+    draw(context) {
+        context.fillStyle = 'grey';
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fill();
     }
 }
