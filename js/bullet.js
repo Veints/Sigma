e29ec9f9d417860e002199d09ce968fd426d4c40
Keller <93058465+Veints@users.noreply.github.com>
2024-10-29 17:06:32 -0700

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -3,7 +3,7 @@ class Bullet {
         this.x = x;
         this.y = y;
         this.direction = direction; // -1 for left, 1 for right
-        this.speed = 5; // Bullet speed
+        this.speed = 10; // Bullet speed (increased)
         this.radius = 5; // Bullet size
     }
 
